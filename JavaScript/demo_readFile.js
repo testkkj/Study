0144e3c9f0bcd6bfd3b73d37d1116e12bb9f59bf
testkkj <46573845+testkkj@users.noreply.github.com>
2021-02-19 14:39:50 +0900

    사내 스터디
    node js 연습

--- JavaScript/demo_readFile.js
+++ JavaScript/demo_readFile.js
@@ -21,4 +21,24 @@ http.createServer(function (req, res) {
     if (err) throw err;
     console.log('Saved!');
   });
+
+  fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
+    if (err) throw err;
+    console.log('Updated!');
+  });
+
+  fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+  });
+  
+  fs.unlink('mynewfile2.txt', function (err) {
+    if (err) throw err;
+    console.log('File deleted!');
+  });
+
+  fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+    if (err) throw err;
+    console.log('File Renamed!');
+  });
 }).listen(8080);
