0660f633d8b55b0940931eb1c1dca5b5a9101ec6
testkkj <46573845+testkkj@users.noreply.github.com>
2021-02-25 22:57:40 +0900

    demoM2
    새로운 스프링 책 시작

---
+++ Java/demoM2/src/test/java/com/example/demoM2/DemoM2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoM2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoM2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
