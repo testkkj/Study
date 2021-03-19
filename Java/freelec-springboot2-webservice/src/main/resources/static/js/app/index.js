d5288de91dd5e0a564ed14c96d41e9176c5d41f3
testkkj <46573845+testkkj@users.noreply.github.com>
2021-03-19 23:03:23 +0900

    수정 오류 수정
    url 오자 v1을 vi로 씀...

--- Java/freelec-springboot2-webservice/src/main/resources/static/js/app/index.js
+++ Java/freelec-springboot2-webservice/src/main/resources/static/js/app/index.js
@@ -38,16 +38,15 @@ var main ={
 
         $.ajax({
             type: 'PUT',
-            url: '/api/vi/posts/'+id,
+            url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
-            console.log(JSON.stringify(error));
         });
     }
 };
