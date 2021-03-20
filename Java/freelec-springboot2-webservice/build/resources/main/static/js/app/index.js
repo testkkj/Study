4acaf92f5993e0957eaf17445ecc7a4415ffad5d
testkkj <46573845+testkkj@users.noreply.github.com>
2021-03-20 23:08:51 +0900

    삭제 기능 추가

--- Java/freelec-springboot2-webservice/build/resources/main/static/js/app/index.js
+++ Java/freelec-springboot2-webservice/build/resources/main/static/js/app/index.js
@@ -7,6 +7,9 @@ var main ={
         $('#btn-update').on('click', function () {
             _this.update();
         });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
     save : function () {
         var data = {
@@ -38,16 +41,30 @@ var main ={
 
         $.ajax({
             type: 'PUT',
-            url: '/api/vi/posts/'+id,
+            url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
-            console.log(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
         });
     }
 };
