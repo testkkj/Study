bd468a9c46d1fca2d480e8f7fe1474a4e7b4a234
testkkj <46573845+testkkj@users.noreply.github.com>
2021-04-12 00:12:47 +0900

    4장 스택
    알고리즘 교재 공부~

---
+++ Java/Algorithm/chap03/X.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+/**
+ * X
+ */
+class X {
+    public static final Comparator<T> COMPARATOR = new Comp();
+
+    private static class Comp implements Comparator<T> {
+        public int compare(T d1 , T d2) {
+            
+        }
+    }
+}
