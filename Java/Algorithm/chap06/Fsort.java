406d76969850a53be64a08b448d63ce30038d3c9
testkkj <46573845+testkkj@users.noreply.github.com>
2021-05-10 23:55:09 +0900

    정렬 알고리즘
    도수 정렬 예제

---
+++ Java/Algorithm/chap06/Fsort.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+/**
+ * Fsort
+ */
+//도수정렬
+public class Fsort {
+    static void fSort(int[] a, int n, int max) {
+        int[] f = new int[max + 1]; //누적 도수
+        int[] b = new int[n]; //작업용 목적 배열
+        
+        for (int i = 0; i < n; i++) { //1단계
+            f[a[i]]++;
+        }
+
+        for (int i = 1; i <= max; i++) { //2단계
+            f[i] += f[i - 1];
+        }
+
+        for (int i = n - 1; i >=  0; i--) { //3단계
+            b[--f[a[i]]] = a[i];
+        }
+
+        for (int i = 0; i < n; i++) { //4단계
+            a[i] = b[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("도수 정렬");
+        System.out.print("요솟수 : ");
+        
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            do {
+                System.out.print("x[" + i + "] : ");
+                x[i] = stdIn.nextInt();
+            } while (x[i] < 0);
+        }
+
+        int max = x[0];
+
+        for (int i = 1; i < nx; i++) {
+            if (x[i] > max) {
+                max = x[i];
+            }
+        }
+
+        fSort(x, nx, max); //배열 x를 도수 정렬
+
+        System.out.println("오름차순으로 정렬했습니다.");
+        for (int i = 0; i < nx; i++) {
+            System.out.println("x[" + i + "] = " + x[i]);
+        }
+    }
+}
