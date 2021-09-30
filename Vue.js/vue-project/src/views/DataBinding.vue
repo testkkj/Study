37aa0ad521f098486e9cf05c00f0ca2793f2c867
testkkj <46573845+testkkj@users.noreply.github.com>
2021-09-30 11:55:49 +0900

    vue 예제
    vscode snippet 설정
    databinding 예제 시작

---
+++ Vue.js/vue-project/src/views/DataBinding.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello, {{title}}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    //html과 자바스크립트 코드에서 사용할 데이터 변수 선언
+    return {
+      title: 'world'
+    };
+  }
+}
+</script>
