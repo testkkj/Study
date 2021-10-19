4dd88dd82f6415ef9b0b2123a68bc29d3bdb69d4
testkkj <46573845+testkkj@users.noreply.github.com>
2021-10-19 22:37:47 +0900

    Vue 예제
    컴포넌트에서  믹스인 사용
    믹스인 파일 전역 등록

---
+++ Vue.js/vue-project/src/views/Mixins.vue
@@ -0,0 +1,18 @@
+<template>
+    <div></div>
+</template>
+<script>
+import ApiMixin from "../api";
+export default {
+    mixins: [ApiMixin],
+    data() {
+        return {
+            productList: []
+        };
+    },
+    async mounted() {
+        this.productList = await this.$callAPI("https://f8cf7ade-5a68-43e8-b6ea-cc3d4d13f483.mock.pstmn.io/list2","get");
+        console.log(this.productList);
+    }
+}
+</script>
