dd5fd65bd9722eac6220a0fb1610ef96e7c7a692
testkkj <46573845+testkkj@users.noreply.github.com>
2021-10-20 23:50:22 +0900

    Vue 예제
    proxy plugin 등 예제
    예제가 명확하게 동작을 보여주지 않아서 미확인

--- Vue.js/vue-project/src/main.js
+++ Vue.js/vue-project/src/main.js
@@ -2,5 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import mixins from './mixins'
+import i18nPlugin from "./plugins/i18n"; //i18n 플러그인 추가
 
-createApp(App).use(router).mixin(mixins).mount('#app')
+const i18nStrings = {
+    en: {
+        hi: 'Hello!'
+    },
+    ko: {
+        hi: '안녕하세요!'
+    }
+}
+
+createApp(App).use(router).mixin(mixins).use(i18nPlugin, i18nStrings).mount('#app') //i18n 플러그인에 다국어 번역 데이터를 파라미터로 전달
+
+app.directive('focus', {
+    mounted(el) {
+        el.focus()
+    }
+})
--- Vue.js/vue-project/vue.config.js
+++ Vue.js/vue-project/vue.config.js
@@ -1,3 +1,16 @@
+// const target = "http://127.0.0.1:3000"; //proxy 요청을 보낼 서버 주소
+
+// module.exports = {
+//     devServer: {
+//         port: 8080,
+//         proxy: { //proxy 요청을 보낼 api 시작 부분
+//             '^api': {
+//                 target,
+//                 changeOrigin: true
+//             }
+//         }
+//     }
+// }
 module.exports = {
     chainWebpack: config => {
         config.plugins.delete('prefetch'); //prefetch 삭제
