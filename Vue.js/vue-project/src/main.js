dee7a9d7352d428f5d1f9f0919480a7fad27c992
testkkj <46573845+testkkj@users.noreply.github.com>
2021-10-21 23:00:11 +0900

    Vue 예제
    Vuex 예제

--- Vue.js/vue-project/src/main.js
+++ Vue.js/vue-project/src/main.js
@@ -15,8 +15,8 @@ const i18nStrings = {
 
 createApp(App).use(router).mixin(mixins).use(i18nPlugin, i18nStrings).mount('#app') //i18n 플러그인에 다국어 번역 데이터를 파라미터로 전달
 
-app.directive('focus', {
-    mounted(el) {
-        el.focus()
-    }
-})
+// app.directive('focus', {
+//     mounted(el) {
+//         el.focus()
+//     }
+// })
---
+++ Vue.js/vue-project/src/views/StoreAccess.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>Count : {{count}}</p>
+    <button type="button">Increment</button>
+</template>
+<script>
+export default {
+    computed: {
+        count() {
+            return this.$store.state.count;
+        },
+        cartCount() {
+            return this.$store.getters.cartCount;
+        }
+    },
+    methods: {
+        increment() {
+            this.$store.commit('increment');
+        }
+    }
+}
+</script>
