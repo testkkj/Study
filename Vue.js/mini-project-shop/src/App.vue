c3d55befbc9756f707c24f47b1359f6f7c1b1230
testkkj <46573845+testkkj@users.noreply.github.com>
2021-10-23 23:00:12 +0900

    Vue 예제
    미니 프로젝트 기본 레이아웃 구조 및 연동 파일

--- Vue.js/mini-project-shop/src/App.vue
+++ Vue.js/mini-project-shop/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header></Header>
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./layouts/Header.vue";
+import Footer from "./layouts/Footer.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: {Header, Footer},
+  computed: {
+    user() {
+      return this.$store.status.user;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
