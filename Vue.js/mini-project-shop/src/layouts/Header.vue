385bc4b1ca041a37e2b77c79feccc3c82157017f
testkkj <46573845+testkkj@users.noreply.github.com>
2021-10-24 21:27:54 +0900

    Vue 예제
    미니 프로젝트 기본 레이아웃 구조 및 연동 파일

--- Vue.js/mini-project-shop/src/layouts/Header.vue
+++ Vue.js/mini-project-shop/src/layouts/Header.vue
@@ -19,6 +19,12 @@
                     <li v-if="user.email != undefined" class="nav-item">
                         <router-link class="nav-link" to="/sales">제품등록페이지</router-link>
                     </li>
+                    <li v-if="user.email==undefined">
+                        <button class="btn btn-danger" type="button" @click="kakaoLogin">로그인</button>
+                    </li>
+                    <li v-else>
+                        <button class="btn btn-danger" type="button" @click="kakaoLogout">로그아웃</button>
+                    </li>
                 </ul>
                 <form action="" class="d-flex">
                     <input type="search" class="form-control me-2" placeholder="Search" aria-label="Search">
@@ -30,6 +36,49 @@
 </template>
 <script>
 export default {
-    name: 'header'
+    name: 'header',
+    computed: {
+        user() {
+            return this.$store.state.user;
+        }
+    },
+    methods: {
+        kakaoLogin() {
+            window.Kakao.Auth.login({
+                scope: 'profile, account_email, gender',
+                success: this.getProfile
+            });
+        },
+        getProfile(authObj) {
+            console.log(authObj);
+            window.Kakao.API.request({
+                url: '/v2/user/me',
+                success: res => {
+                    const kakao_account = res.kakao_account;
+                    console.log(kakao_account);
+                    this.login(kakao_account);
+                    alert("로그인 성공!");
+                }
+            });
+        },
+        async login(kakao_account) {
+            await this.$api("/api/login", {
+                param: [
+                    {email:kakao_account.email, nickname:kakao_account.profile.nickname},
+                    {nickname:kakao_account.profile.nickname}
+                ]
+            });
+
+            this.$store.commit("user", kakao_account);
+        },
+        kakaoLgoout() {
+            window.Kakao.Auth.logout((response) => {
+                console.log(response);
+                this.$this.commit("user", {});
+                alert("로그아웃");
+                this.$router.push({path:'/'});
+            });
+        }
+    }
 }
 </script>
--- Vue.js/mini-project-shop/src/main.js
+++ Vue.js/mini-project-shop/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import mixins from './mixins'
+import store from './store'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mixin(mixins).use(store).use(VueSweetalert2).mount('#app')
+
+window.KaKao.init("dd0fe2d1d9bbf02289024d14c5f8e31c");
