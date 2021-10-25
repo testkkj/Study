d2e12f416d30be089174c78c7067c148e2e65192
testkkj <46573845+testkkj@users.noreply.github.com>
2021-10-25 22:34:22 +0900

    Vue 예제
    미니 프로젝트 기본 레이아웃 구조 및 연동 파일

---
+++ Vue.js/mini-project-shop/src/views/productDetail.vue
@@ -0,0 +1,61 @@
+<template>
+    <main class="mt-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div id="carouselExampleIndicators" class="carousel caousel-dark slide" data-bs-ride="carousel">
+                        <ol class="crousel-indicators">
+                            <li class="active" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"></li>
+                            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+                            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div :class="`carousel-item ${i==0?'active':''}`" :key="i" v-for="(pimg, i) in productImage">
+                                <img :src="`download/${productId}/${pimg.path}`" alt="" class="d-block">
+                            </div>
+                        </div>
+                        <a href="#carouselExampleIndivators" class="carousel-control-prev" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a href="#carouselExampleIndivators" class="carousel-control-next" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">{{productDetail.product_name}}</h5>
+                            <h5 class="card-title pt-3 pb-3 border-top">{{getCurrencyFormat(productDetail.product_price)}}원</h5>
+                            <p class="card-text border-top pt-3">
+                                <span class="badge bg-dark me-1">{{productDetail.category1}}</span>
+                                <span class="badge bg-dark me-1">{{productDetail.category2}}</span>
+                                <span class="badge bg-dark">{{productDetail.category3}}</span>
+                            </p>
+                            <p class="card-text pb-3">
+                                배송비{{getCurrencyFormat(productDetail.delivery_price)}}원 | 도서산간(제주도) 배송비 추가 {{getCurrencyFormat(productDetail.add_delivery_price)}}원 | 택배배송 | {{productDetail.outbound_days}}일 이내 출고 (주말, 공휴일 제외)
+                            </p>
+                            <div class="card-text border-top pb-3">
+                                <div class="row">
+                                    <div class="col-auto">
+                                        <label for="" class="col-form-label">구매수량</label>
+                                    </div>
+                                    <div class="cal-auto">
+                                        <div class="input-group">
+                                            <span class="input-group-text" style="cursor:pointer;" @click="calculatePrice(-1);">-</span>
+                                            <input type="text" class="form-control" style="width:40px;" v-model="total">
+                                            <span class="input-group-text" style="cursor:pointer;" @click="calculatePrice(1);">+</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row pt-3 pb-3 border-top"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
