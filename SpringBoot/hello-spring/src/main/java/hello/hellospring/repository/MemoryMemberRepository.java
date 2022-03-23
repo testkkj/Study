5b035a70806847ff7151b240498febd801436d05
testkkj <46573845+testkkj@users.noreply.github.com>
2022-03-23 21:56:51 +0900

    Spring Boot
    10장 테스트 케이스 예제 오류 수정
    - 원본 메서드의 리턴값을 잘못 작성해서 발생한 오류

--- SpringBoot/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ SpringBoot/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -27,8 +27,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findByName(String name) {
-        store.values().stream().filter(member -> member.getName().equals(name)).findAny();
-        return null;
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
     }
 
     @Override
